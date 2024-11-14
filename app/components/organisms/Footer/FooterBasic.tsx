d100fd7a6a0363658d405736fa3a74437bd9fae8
RDavis830 <rd114348@hc.ac.je>
2024-11-14 16:28:09 +0000

    Links & size adjustment testing

--- app/components/organisms/Footer/FooterBasic.tsx
+++ app/components/organisms/Footer/FooterBasic.tsx
@@ -1,3 +1,4 @@
+import { Link } from '@remix-run/react'
 import logo from '#/app/assets/png/Logo.png'
 
 export interface FooterProps {
@@ -13,11 +14,52 @@ const FooterBasic = ({
 		<footer className="dark:bg-dark-secondary bg-secondary py-16">
 			<div className="container flex flex-col items-center justify-between gap-y-8 lg:flex-row">
 				<div className="w-20">
-					<img src={logo} alt={'Nnews Logo'} />
+					<Link to="/">
+						<img src={logo} alt={'Nnews Logo'} />
+					</Link>
+				</div>
+				<div>
+					<Link
+						to="/news"
+						className="text-sm font-semibold text-muted-foreground transition hover:drop-shadow-md"
+					>
+						Business
+					</Link>
+				</div>
+				<div>
+					<Link
+						to="/news"
+						className="text-sm font-semibold text-muted-foreground transition hover:drop-shadow-md"
+					>
+						Sport
+					</Link>
+				</div>
+				<div>
+					<Link
+						to="/news"
+						className="text-sm font-semibold text-muted-foreground transition hover:drop-shadow-md"
+					>
+						Weather
+					</Link>
+				</div>
+				<div>
+					<Link
+						to="/news"
+						className="text-sm font-semibold text-muted-foreground transition hover:drop-shadow-md"
+					>
+						Tech
+					</Link>
 				</div>
 
 				<div className="dark:text-dark-muted-foreground text-xs text-muted-foreground text-opacity-50">
 					&copy; {companyName} | {new Date().getFullYear()}
+				</div>
+			</div>
+			<div className="mt-10 flex justify-center">
+				<div className=" dark:text-dark-muted-foreground flex text-center text-xs text-muted-foreground text-opacity-50">
+					All content on this website is the exclusive property of Nnews limited
+					and protected by copyright laws. Any unauthorized use, reproduction,
+					or distribution of this content may result in legal action.
 				</div>
 			</div>
 		</footer>
--- app/components/organisms/HeaderWithSearch.tsx
+++ app/components/organisms/HeaderWithSearch.tsx
@@ -1,4 +1,4 @@
-import { useMatches } from '@remix-run/react'
+import { Link, useMatches } from '@remix-run/react'
 import logo from '~/assets/png/Logo.png'
 import { SearchBar } from '../molecules/SearchBar'
 import LoginOrUserDropdown from './LoginOrUserDropdown'
@@ -11,8 +11,42 @@ export default function HeaderWithSearch() {
 	return (
 		<header className="dark:bg-dark-primary/10 bg-primary/10 py-6">
 			<nav className="container flex flex-wrap items-center justify-between gap-4 sm:flex-nowrap md:gap-8">
+				<Link to="/">
+					<div>
+						<img src={logo} alt="Nnews Logo" className="flex max-w-28" />
+					</div>
+				</Link>
 				<div>
-					<img src={logo} alt="Nnews Logo" className="w-44" />
+					<Link
+						to="/news"
+						className="text-lg font-semibold text-muted-foreground transition hover:drop-shadow-md"
+					>
+						Business
+					</Link>
+				</div>
+				<div>
+					<Link
+						to="/news"
+						className="text-lg font-semibold text-muted-foreground transition hover:drop-shadow-md"
+					>
+						Sport
+					</Link>
+				</div>
+				<div>
+					<Link
+						to="/news"
+						className="text-lg font-semibold text-muted-foreground transition hover:drop-shadow-md"
+					>
+						Weather
+					</Link>
+				</div>
+				<div>
+					<Link
+						to="/news"
+						className="text-lg font-semibold text-muted-foreground transition hover:drop-shadow-md"
+					>
+						Tech
+					</Link>
 				</div>
 				<div className="ml-auto hidden max-w-sm flex-1 sm:block">
 					{searchBar}
